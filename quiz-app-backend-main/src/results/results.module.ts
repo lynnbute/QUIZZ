b32bf2bed35a9d68338acc6cc3f0b0f9fab234da
Nile2Naija-Devs <nile2naija25@gmail.com>
2025-02-09 15:50:37 +0300

    Delete quiz-app-backend-main directory

--- quiz-app-backend-main/src/results/results.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ResultsController } from './results.controller';
-import { ResultsService } from './results.service';
-
-@Module({
-  controllers: [ResultsController],
-  providers: [ResultsService]
-})
-export class ResultsModule {}
